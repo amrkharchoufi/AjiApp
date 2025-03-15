908b26372ca2703eb8ef1450da93e1273fec975b
amrkharchoufi <amrkharchoufi3@gmail.com>
2025-03-15 21:25:42 +0000

    added loginfunction

--- lib/views/login_view.dart
+++ lib/views/login_view.dart
@@ -1,6 +1,6 @@
+import 'package:ajiapp/backend/backend.dart';
 import 'package:ajiapp/settings/colors.dart';
 import 'package:ajiapp/settings/size.dart';
-import 'package:ajiapp/views/ClientSpace.dart';
 import 'package:ajiapp/views/SignUp_view.dart';
 import 'package:ajiapp/views/forgotpassword_view.dart';
 import 'package:flutter/material.dart';
@@ -16,6 +16,7 @@ class LoginView extends StatefulWidget {
 class _LoginViewState extends State<LoginView> {
   final TextEditingController _email = TextEditingController();
   final TextEditingController _pwd = TextEditingController();
+  GlobalKey<FormState> mykey = GlobalKey();
   bool visible = true;
   bool remeberme = false;
   @override
@@ -49,162 +50,177 @@ class _LoginViewState extends State<LoginView> {
                   topLeft: Radius.circular(ScreenSize.width * 0.1),
                   topRight: Radius.circular(ScreenSize.width * 0.1)),
             ),
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.spaceAround,
-              children: [
-                Text(
-                  'Login with email address',
-                  style: TextStyle(
-                      fontSize: ScreenSize.width / 17,
-                      fontWeight: FontWeight.bold,
-                      fontFamily: "SFDisplay"),
-                ),
-                SizedBox(
-                  height: ScreenSize.height / 40,
-                ),
-                TextField(
-                  controller: _email,
-                  decoration: InputDecoration(
-                    labelText: 'Email Address',
-                    prefixIcon: Icon(Icons.email_outlined), // Email icon
-                    suffixIcon: _email.text.isNotEmpty
-                        ? IconButton(
-                            icon: Icon(Icons.clear),
-                            onPressed: () {
-                              _email.clear();
-                              setState(() {});
-                            },
-                          )
-                        : null,
-                    border: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(10.0),
-                      borderSide: BorderSide(color: Colors.black),
-                    ),
-                  ),
-                ),
-                TextField(
-                  controller: _pwd,
-                  obscureText: visible,
-                  decoration: InputDecoration(
-                    labelText: 'Password',
-                    prefixIcon: Icon(Icons.lock_outline), // Email icon
-                    suffixIcon: _pwd.text.isNotEmpty
-                        ? IconButton(
-                            icon: Icon(Icons.visibility),
-                            onPressed: () {
-                              visible = !visible;
-                              setState(() {});
-                            },
-                          )
-                        : null,
-                    border: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(10.0),
-                      borderSide: BorderSide(color: Colors.black),
-                    ),
-                  ),
-                ),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    Row(
-                      children: [
-                        Checkbox(
-                            activeColor: ajired,
-                            value: remeberme,
-                            onChanged: (bool? value) {
-                              remeberme = value!;
-                              setState(() {});
-                            }),
-                        Text(
-                          'Remember me',
-                          style: TextStyle(color: Colors.black45),
-                        )
-                      ],
-                    ),
-                    TextButton(
-                        onPressed: () {
-                          Navigator.push(
-                            context,
-                            MaterialPageRoute(
-                                builder: (context) => ForgotpasswordView()),
-                          );
-                        },
-                        child: Text(
-                          'Forgot Password?',
-                          style: TextStyle(color: ajired),
-                        )),
-                  ],
-                ),
-                ElevatedButton(
-                  onPressed: () {
-                    Navigator.pop(context);
-                    Navigator.push(
-                      context,
-                      MaterialPageRoute(builder: (context) => Clientspace()),
-                    );
-                  },
-                  style: ElevatedButton.styleFrom(
-                    backgroundColor: ajired, // Custom color
-                    shape: RoundedRectangleBorder(
-                      borderRadius:
-                          BorderRadius.circular(20.0), // Rounded edges
-                    ),
-                    padding: EdgeInsets.symmetric(
-                        horizontal: ScreenSize.width / 4,
-                        vertical: ScreenSize.width / 28), // Button size
-                  ),
-                  child: Text(
-                    "Continue",
+            child: Form(
+              key: mykey,
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                children: [
+                  Text(
+                    'Login with email address',
                     style: TextStyle(
-                      color: Colors.white, // White text
-                      fontSize: ScreenSize.width / 20, // Text size
-                      fontWeight: FontWeight.bold,
+                        fontSize: ScreenSize.width / 17,
+                        fontWeight: FontWeight.bold,
+                        fontFamily: "SFDisplay"),
+                  ),
+                  SizedBox(
+                    height: ScreenSize.height / 40,
+                  ),
+                  TextFormField(
+                    controller: _email,
+                    validator: (value) {
+                      if (value == null || value.isEmpty) {
+                        return '*E-mail Required';
+                      }
+                      return null;
+                    },
+                    decoration: InputDecoration(
+                      labelText: 'Email Address',
+                      prefixIcon: Icon(Icons.email_outlined), // Email icon
+                      suffixIcon: _email.text.isNotEmpty
+                          ? IconButton(
+                              icon: Icon(Icons.clear),
+                              onPressed: () {
+                                _email.clear();
+                                setState(() {});
+                              },
+                            )
+                          : null,
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(10.0),
+                        borderSide: BorderSide(color: Colors.black),
+                      ),
                     ),
                   ),
-                ),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    Text(
-                      "Doesn't have an account?",
-                      style: TextStyle(
-                          color: Colors.black45,
-                          fontSize: ScreenSize.width / 35),
+                  TextFormField(
+                    controller: _pwd,
+                    validator: (value) {
+                      if (value == null || value.isEmpty) {
+                        return '*Password Required';
+                      }
+                      return null;
+                    },
+                    obscureText: visible,
+                    decoration: InputDecoration(
+                      labelText: 'Password',
+                      prefixIcon: Icon(Icons.lock_outline), // Email icon
+                      suffixIcon: _pwd.text.isNotEmpty
+                          ? IconButton(
+                              icon: Icon(Icons.visibility),
+                              onPressed: () {
+                                visible = !visible;
+                                setState(() {});
+                              },
+                            )
+                          : null,
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(10.0),
+                        borderSide: BorderSide(color: Colors.black),
+                      ),
                     ),
-                    TextButton(
-                        onPressed: () {
-                          Navigator.push(
-                            context,
-                            MaterialPageRoute(
-                                builder: (context) => SignupView()),
-                          );
-                        },
-                        child: Text(
-                          'Sign Up',
-                          style: TextStyle(
-                              color: ajired, fontSize: ScreenSize.width / 35),
-                        ))
-                  ],
-                ),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceAround,
-                  children: [
-                    SizedBox(width: ScreenSize.width / 3, child: Divider()),
-                    Text(
-                      'or',
-                      style: TextStyle(color: Colors.black45),
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Row(
+                        children: [
+                          Checkbox(
+                              activeColor: ajired,
+                              value: remeberme,
+                              onChanged: (bool? value) {
+                                remeberme = value!;
+                                setState(() {});
+                              }),
+                          Text(
+                            'Remember me',
+                            style: TextStyle(color: Colors.black45),
+                          )
+                        ],
+                      ),
+                      TextButton(
+                          onPressed: () {
+                            Navigator.push(
+                              context,
+                              MaterialPageRoute(
+                                  builder: (context) => ForgotpasswordView()),
+                            );
+                          },
+                          child: Text(
+                            'Forgot Password?',
+                            style: TextStyle(color: ajired),
+                          )),
+                    ],
+                  ),
+                  ElevatedButton(
+                    onPressed: () {
+                      if (mykey.currentState!.validate()) {
+                        setState(() {
+                          login(context, _email.text.trim(), _pwd.text.trim());
+                        });
+                      }
+                    },
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor: ajired, // Custom color
+                      shape: RoundedRectangleBorder(
+                        borderRadius:
+                            BorderRadius.circular(20.0), // Rounded edges
+                      ),
+                      padding: EdgeInsets.symmetric(
+                          horizontal: ScreenSize.width / 4,
+                          vertical: ScreenSize.width / 28), // Button size
                     ),
-                    SizedBox(width: ScreenSize.width / 3, child: Divider()),
-                  ],
-                ),
-                TextButton(
-                    onPressed: () {},
                     child: Text(
-                      'Login with Google Account',
+                      "Continue",
                       style: TextStyle(
-                          color: ajired, fontSize: ScreenSize.width / 35),
-                    ))
-              ],
+                        color: Colors.white, // White text
+                        fontSize: ScreenSize.width / 20, // Text size
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Text(
+                        "Doesn't have an account?",
+                        style: TextStyle(
+                            color: Colors.black45,
+                            fontSize: ScreenSize.width / 35),
+                      ),
+                      TextButton(
+                          onPressed: () {
+                            Navigator.push(
+                              context,
+                              MaterialPageRoute(
+                                  builder: (context) => SignupView()),
+                            );
+                          },
+                          child: Text(
+                            'Sign Up',
+                            style: TextStyle(
+                                color: ajired, fontSize: ScreenSize.width / 35),
+                          ))
+                    ],
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceAround,
+                    children: [
+                      SizedBox(width: ScreenSize.width / 3, child: Divider()),
+                      Text(
+                        'or',
+                        style: TextStyle(color: Colors.black45),
+                      ),
+                      SizedBox(width: ScreenSize.width / 3, child: Divider()),
+                    ],
+                  ),
+                  TextButton(
+                      onPressed: () {},
+                      child: Text(
+                        'Login with Google Account',
+                        style: TextStyle(
+                            color: ajired, fontSize: ScreenSize.width / 35),
+                      ))
+                ],
+              ),
             ),
           ),
         ],
