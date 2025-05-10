905c0faef2e96cd6b038b46e07e97b1fd0a63bc3
unknown <amrkharchoufi3@gmail.com>
2025-05-10 22:03:19 +0100

    modification on hompage and map

--- lib/services/common/Map_view.dart
+++ lib/services/common/Map_view.dart
@@ -1,5 +1,8 @@
+import 'package:ajiapp/routing.dart';
+import 'package:ajiapp/settings/colors.dart';
 import 'package:ajiapp/settings/size.dart';
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 
 class MapView extends StatefulWidget {
@@ -15,10 +18,31 @@ class _MapViewState extends State<MapView> {
   Widget build(BuildContext context) {
     ScreenSize.init(context);
     return Scaffold(
-      appBar: AppBar(),
+      appBar: AppBar(
+        backgroundColor: Colors.white,
+        actions: [
+          IconButton(
+            onPressed: () {
+              Get.toNamed(Routes.NOTIFICATION);
+            },
+            icon: Icon(
+              Icons.notifications_outlined,
+              color: ajired,
+              size: ScreenSize.width / 13,
+            ),
+          ),
+          IconButton(
+            onPressed: () {},
+            icon: Icon(
+              Icons.account_circle_outlined,
+              color: ajired,
+              size: ScreenSize.width / 12,
+            ),
+          ),
+        ],
+      ),
       body: GoogleMap(
-          initialCameraPosition:
-              CameraPosition(target: _myhouse, zoom: 15),
+          initialCameraPosition: CameraPosition(target: _myhouse, zoom: 15),
           cloudMapId: 'cd31d568b85381c8e142ab30'),
     );
   }
