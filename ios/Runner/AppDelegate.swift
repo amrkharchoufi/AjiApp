9d71947204e148e0cf2e32a747ae40bf15f752b2
amrkharchoufi <amrkharchoufi3@gmail.com>
2025-06-21 21:17:39 +0100

    fixing apple

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -1,7 +1,5 @@
-import UIKit
 import Flutter
-import Firebase
-import GoogleSignIn
+import UIKit
 import GoogleMaps
 
 @main
@@ -10,21 +8,8 @@ import GoogleMaps
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
-    // Initialize Firebase
-    FirebaseApp.configure()
-
-    // Provide Google Maps API key
     GMSServices.provideAPIKey("AIzaSyDN64-ls0P9ffZhc8SD9FeZI0Edr-8zC94")
-
-    // Register plugins
     GeneratedPluginRegistrant.register(with: self)
-
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
-  }
-
-  // Handle Google Sign-In redirect
-  override func application(_ app: UIApplication, open url: URL,
-                            options: [UIApplication.OpenURLOptionsKey : Any] = [:]) -> Bool {
-    return GIDSignIn.sharedInstance.handle(url)
   }
 }
