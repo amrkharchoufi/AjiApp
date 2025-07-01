e0c67c03ebb10e301043692010ce083a7a2c042c
amrkharchoufi <amrkharchoufi3@gmail.com>
2025-07-01 23:13:23 +0100

    stadium detail

--- lib/services/followyourteam_service/view/stadium_details_view.dart
+++ lib/services/followyourteam_service/view/stadium_details_view.dart
@@ -51,165 +51,72 @@ class StadiumDetailsView extends StatelessWidget {
                           fontWeight: FontWeight.bold,
                           fontSize: SizeConfig.getBlockSizeHorizontal(6)),
                     ),
-                    Column(
-                      mainAxisAlignment: MainAxisAlignment.start,
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      spacing: 10,
-                      children: [
-                        SizedBox(
-                          width: Get.width,
-                          child: Stack(
-                            children: [
-                              CarouselSlider(
-                                carouselController: carouselSliderController,
-                                options: CarouselOptions(
-                                    height:
-                                        SizeConfig.getBlockSizeHorizontal(68),
-                                    viewportFraction: 1),
-                                items:
-                                    [controller.selectedstd.imageUrl].map((i) {
-                                  return Builder(
-                                    builder: (BuildContext context) {
-                                      return CachedNetworkImage(
-                                        imageUrl: i,
-                                        fit: BoxFit.cover,
-                                      );
-                                    },
+                    Container(
+                      clipBehavior: Clip.hardEdge,
+                      decoration:
+                          BoxDecoration(borderRadius: BorderRadius.circular(5)),
+                      width: Get.width,
+                      child: Stack(
+                        children: [
+                          CarouselSlider(
+                            carouselController: carouselSliderController,
+                            options: CarouselOptions(
+                                height: SizeConfig.getBlockSizeHorizontal(68),
+                                viewportFraction: 1),
+                            items: [controller.selectedstd.imageUrl].map((i) {
+                              return Builder(
+                                builder: (BuildContext context) {
+                                  return CachedNetworkImage(
+                                    imageUrl: i,
+                                    fit: BoxFit.cover,
                                   );
-                                }).toList(),
-                              ),
-                              Positioned(
-                                right: 0,
-                                top: 0,
-                                bottom: 0,
-                                child: IconButton(
-                                  onPressed: () {
-                                    carouselSliderController.nextPage();
-                                  },
-                                  icon: Container(
-                                    decoration: BoxDecoration(
-                                        border: Border.all(
-                                            color: Colors.white, width: 2),
-                                        shape: BoxShape.circle),
-                                    child: Center(
-                                      child: Icon(
-                                        Icons.arrow_forward,
-                                        color: Colors.white,
-                                        size: 18,
-                                      ),
-                                    ),
+                                },
+                              );
+                            }).toList(),
+                          ),
+                          Positioned(
+                            right: 0,
+                            top: 0,
+                            bottom: 0,
+                            child: IconButton(
+                              onPressed: () {
+                                carouselSliderController.nextPage();
+                              },
+                              icon: Container(
+                                decoration: BoxDecoration(
+                                    border: Border.all(
+                                        color: Colors.white, width: 2),
+                                    shape: BoxShape.circle),
+                                child: Center(
+                                  child: Icon(
+                                    Icons.arrow_forward,
+                                    color: Colors.white,
+                                    size: 18,
                                   ),
                                 ),
                               ),
-                              Positioned(
-                                left: 0,
-                                top: 0,
-                                bottom: 0,
-                                child: IconButton(
-                                  onPressed: () {
-                                    carouselSliderController.previousPage();
-                                  },
-                                  icon: Container(
-                                    decoration: BoxDecoration(
-                                        border: Border.all(
-                                            color: Colors.white, width: 2),
-                                        shape: BoxShape.circle),
-                                    child: Icon(
-                                      Icons.arrow_back,
-                                      color: Colors.white,
-                                      size: 18,
-                                    ),
-                                  ),
+                            ),
+                          ),
+                          Positioned(
+                            left: 0,
+                            top: 0,
+                            bottom: 0,
+                            child: IconButton(
+                              onPressed: () {
+                                carouselSliderController.previousPage();
+                              },
+                              icon: Container(
+                                decoration: BoxDecoration(
+                                    border: Border.all(
+                                        color: Colors.white, width: 2),
+                                    shape: BoxShape.circle),
+                                child: Icon(
+                                  Icons.arrow_back,
+                                  color: Colors.white,
+                                  size: 18,
                                 ),
                               ),
-                            ],
-                          ),
-                        )
-                      ],
-                    ),
-                    Padding(
-                      padding: EdgeInsets.symmetric(
-                          horizontal: SizeConfig.getBlockSizeHorizontal(5)),
-                      child: Row(
-                        spacing: SizeConfig.getBlockSizeHorizontal(5),
-                        children: [
-                          Column(
-                            spacing: SizeConfig.getBlockSizeVertical(1.5),
-                            crossAxisAlignment: CrossAxisAlignment.start,
-                            children: [
-                              Text(
-                                "Location:",
-                                style: TextStyle(
-                                    fontWeight: FontWeight.bold,
-                                    fontSize:
-                                        SizeConfig.getBlockSizeHorizontal(4)),
-                              ),
-                              Text(
-                                "Capacity:",
-                                style: TextStyle(
-                                    fontWeight: FontWeight.bold,
-                                    fontSize:
-                                        SizeConfig.getBlockSizeHorizontal(4)),
-                              ),
-                              Text(
-                                "Inauguration:",
-                                style: TextStyle(
-                                    fontWeight: FontWeight.bold,
-                                    fontSize:
-                                        SizeConfig.getBlockSizeHorizontal(4)),
-                              ),
-                              Text(
-                                "Home ground for:",
-                                style: TextStyle(
-                                    fontWeight: FontWeight.bold,
-                                    fontSize:
-                                        SizeConfig.getBlockSizeHorizontal(4)),
-                              ),
-                            ],
-                          ),
-                          Column(
-                            spacing: SizeConfig.getBlockSizeVertical(1.5),
-                            crossAxisAlignment: CrossAxisAlignment.start,
-                            children: [
-                              Text(
-                                controller.selectedstd.city,
-                                style: TextStyle(
-                                    fontWeight: FontWeight.w500,
-                                    fontSize:
-                                        SizeConfig.getBlockSizeHorizontal(4)),
-                              ),
-                              Text(controller.selectedstd.capacity,
-                                  style: TextStyle(
-                                      fontWeight: FontWeight.w500,
-                                      fontSize:
-                                          SizeConfig.getBlockSizeHorizontal(
-                                              4))),
-                              Text(controller.selectedstd.inauguration,
-                                  style: TextStyle(
-                                      fontWeight: FontWeight.w500,
-                                      fontSize:
-                                          SizeConfig.getBlockSizeHorizontal(
-                                              4))),
-                              Text(controller.selectedstd.homeGroundFor,
-                                  style: TextStyle(
-                                      fontWeight: FontWeight.w500,
-                                      fontSize:
-                                          SizeConfig.getBlockSizeHorizontal(4)))
-                            ],
-                          )
-                        ],
-                      ),
-                    ),
-                    Padding(
-                      padding: EdgeInsets.symmetric(
-                          horizontal: SizeConfig.getBlockSizeHorizontal(5)),
-                      child: Row(
-                        children: [
-                          Text(
-                            "About Stadium",
-                            style: TextStyle(
-                                fontWeight: FontWeight.bold,
-                                fontSize: SizeConfig.getBlockSizeHorizontal(6)),
+                            ),
                           ),
                         ],
                       ),
@@ -217,30 +124,117 @@ class StadiumDetailsView extends StatelessWidget {
                     Padding(
                       padding: EdgeInsets.symmetric(
                           horizontal: SizeConfig.getBlockSizeHorizontal(5)),
-                      child: Text(controller.selectedstd.description),
-                    ),
-                    Padding(
-                      padding: EdgeInsets.symmetric(
-                          horizontal: SizeConfig.getBlockSizeHorizontal(5)),
-                      child: Row(
-                        mainAxisAlignment: MainAxisAlignment.end,
+                      child: Column(
+                        spacing: SizeConfig.getBlockSizeVertical(1.5),
                         children: [
-                          MaterialButton(
-                            onPressed: () {},
-                            color: ajired,
-                            shape: RoundedRectangleBorder(
-                              borderRadius: BorderRadius.circular(10),
-                            ),
-                            minWidth: SizeConfig.getBlockSizeHorizontal(35),
-                            height: SizeConfig.getBlockSizeVertical(3),
-                            child: Text(
-                              "View on map",
-                              style: TextStyle(
-                                color: Colors.white,
-                                fontSize:
-                                    SizeConfig.getBlockSizeHorizontal(3.5),
+                          Row(
+                            spacing: SizeConfig.getBlockSizeHorizontal(5),
+                            children: [
+                              Column(
+                                spacing: SizeConfig.getBlockSizeVertical(1.5),
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                children: [
+                                  Text(
+                                    "Location:",
+                                    style: TextStyle(
+                                        fontWeight: FontWeight.bold,
+                                        fontSize:
+                                            SizeConfig.getBlockSizeHorizontal(
+                                                4)),
+                                  ),
+                                  Text(
+                                    "Capacity:",
+                                    style: TextStyle(
+                                        fontWeight: FontWeight.bold,
+                                        fontSize:
+                                            SizeConfig.getBlockSizeHorizontal(
+                                                4)),
+                                  ),
+                                  Text(
+                                    "Inauguration:",
+                                    style: TextStyle(
+                                        fontWeight: FontWeight.bold,
+                                        fontSize:
+                                            SizeConfig.getBlockSizeHorizontal(
+                                                4)),
+                                  ),
+                                  Text(
+                                    "Home ground for:",
+                                    style: TextStyle(
+                                        fontWeight: FontWeight.bold,
+                                        fontSize:
+                                            SizeConfig.getBlockSizeHorizontal(
+                                                4)),
+                                  ),
+                                ],
                               ),
-                            ),
+                              Column(
+                                spacing: SizeConfig.getBlockSizeVertical(1.5),
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                children: [
+                                  Text(
+                                    controller.selectedstd.city,
+                                    style: TextStyle(
+                                        fontWeight: FontWeight.w500,
+                                        fontSize:
+                                            SizeConfig.getBlockSizeHorizontal(
+                                                4)),
+                                  ),
+                                  Text(controller.selectedstd.capacity,
+                                      style: TextStyle(
+                                          fontWeight: FontWeight.w500,
+                                          fontSize:
+                                              SizeConfig.getBlockSizeHorizontal(
+                                                  4))),
+                                  Text(controller.selectedstd.inauguration,
+                                      style: TextStyle(
+                                          fontWeight: FontWeight.w500,
+                                          fontSize:
+                                              SizeConfig.getBlockSizeHorizontal(
+                                                  4))),
+                                  Text(controller.selectedstd.homeGroundFor,
+                                      style: TextStyle(
+                                          fontWeight: FontWeight.w500,
+                                          fontSize:
+                                              SizeConfig.getBlockSizeHorizontal(
+                                                  4)))
+                                ],
+                              )
+                            ],
+                          ),
+                          Row(
+                            children: [
+                              Text(
+                                "About Stadium",
+                                style: TextStyle(
+                                    fontWeight: FontWeight.bold,
+                                    fontSize:
+                                        SizeConfig.getBlockSizeHorizontal(6)),
+                              ),
+                            ],
+                          ),
+                          Text(controller.selectedstd.description),
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.end,
+                            children: [
+                              MaterialButton(
+                                onPressed: () {},
+                                color: ajired,
+                                shape: RoundedRectangleBorder(
+                                  borderRadius: BorderRadius.circular(10),
+                                ),
+                                minWidth: SizeConfig.getBlockSizeHorizontal(35),
+                                height: SizeConfig.getBlockSizeVertical(3),
+                                child: Text(
+                                  "View on map",
+                                  style: TextStyle(
+                                    color: Colors.white,
+                                    fontSize:
+                                        SizeConfig.getBlockSizeHorizontal(3.5),
+                                  ),
+                                ),
+                              ),
+                            ],
                           ),
                         ],
                       ),
